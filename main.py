c56a4161f1ad63e6af8c554ff7a9295ba72fe74a
sergey080888 <237264@mail.ru>
2022-06-18 09:40:25 +0300

    del

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 file_name = 'recipes.txt'
 def file_opener(file_name):
     with open(file_name) as text:
-        data = text.read()
-        print(data)
+        cook_book = {}
+        for line in text:
+            food_name = line
+
+            quantity = text.readline()
+            cook_book[food_name] = []
+            for item in range(int(quantity)):
+                cook_book[food_name].append({'ingredient_name':0, 'quantity':0, 'measure':0})
+            text.readline()
+
+    return(cook_book)
 
 
-file_opener(file_name)
+print(file_opener(file_name))
