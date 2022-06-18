f18b7c7b58b6b3682fb867247de87af7b26e961e
sergey080888 <237264@mail.ru>
2022-06-18 09:41:24 +0300

    Revert "del"
    This reverts commit c56a4161f1ad63e6af8c554ff7a9295ba72fe74a.

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 file_name = 'recipes.txt'
 def file_opener(file_name):
     with open(file_name) as text:
-        cook_book = {}
-        for line in text:
-            food_name = line
-
-            quantity = text.readline()
-            cook_book[food_name] = []
-            for item in range(int(quantity)):
-                cook_book[food_name].append({'ingredient_name':0, 'quantity':0, 'measure':0})
-            text.readline()
-
-    return(cook_book)
+        data = text.read()
+        print(data)
 
 
-print(file_opener(file_name))
+file_opener(file_name)
