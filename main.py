f3a3e793236dcd4a525d1efcde83a360ddd2514c
sergey080888 <237264@mail.ru>
2022-06-18 16:19:32 +0300

    задание 1

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+
 file_name = 'recipes.txt'
 def file_opener(file_name):
     with open(file_name) as text:
-        data = text.read()
-        print(data)
+        cook_book = {}
+        for line in text:
+            food_name = line.strip()
+            ingredients = []
+            quantity_food = text.readline()
+            for item in range(int(quantity_food)):
+                ingredient = text.readline()
+                ingredient_list = ingredient.split('|')
+                ingredient_dict = {'ingredient_name': 0, 'quantity': 0, 'measure': 0 }
+                n = 0
+                for key in ingredient_dict:
+                    if n == 1:
+                        ingredient_dict[key] = int(ingredient_list[n])
+                    else:
+                        ingredient_dict[key] = ingredient_list[n].strip()
+                    n += 1
+                ingredients.append(ingredient_dict)
+            cook_book[food_name] = ingredients
+            text.readline()
+        return cook_book
 
 
-file_opener(file_name)
+# measure и quantity поменять местами
+
+print(file_opener(file_name))
